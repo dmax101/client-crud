1375a67d498f6c810787fcee6925efed9e080c4a
Danilo Ribeiro <danriba@gmail.com>
2024-01-03 21:11:02 -0300

    create basic navbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [CommonModule, RouterOutlet, HttpClientModule, NavbarComponent],
 })
 export class AppComponent {
-  title = 'client-crud';
+  title = 'Cliente Crud';
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,43 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Montserrat", "sans-serif"],
+      },
+      colors: {
+        "safra-primary": {
+          900: "#002C37",
+          500: "#00586F",
+          100: "#7FABB7",
+        },
+        "safra-secondary": {
+          900: "#7A6019",
+          500: "#F5C132",
+          100: "#FBE6AD",
+        },
+        "safra-success": {
+          900: "#225230",
+          500: "#44A45F",
+          100: "#DAEDDF",
+        },
+        "safra-warning": {
+          900: "#736A11",
+          500: "#E6D422",
+          100: "#FAF6D3",
+        },
+        "safra-danger": {
+          900: "#730C1F",
+          500: "#E6173E",
+          100: "#FAD1D8",
+        },
+        "safra-info": {
+          900: "#1E536B",
+          500: "#3CA6D6",
+          100: "#D8EDF7",
+        },
+      },
+    },
   },
   plugins: [],
 };
