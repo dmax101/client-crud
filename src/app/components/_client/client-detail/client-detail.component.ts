33055e0e743c9bdcd460810ccd19d95bc62500a3
Danilo Ribeiro <danriba@gmail.com>
2024-01-04 09:48:38 -0300

    cpf validator

--- src/app/components/_client/client-detail/client-detail.component.ts
+++ src/app/components/_client/client-detail/client-detail.component.ts
@@ -8,6 +8,7 @@ import {
   Validators,
 } from '@angular/forms';
 import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-client-detail',
@@ -15,6 +16,7 @@ import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
   templateUrl: './client-detail.component.html',
   styleUrl: './client-detail.component.scss',
   imports: [
+    CommonModule,
     InputComponent,
     ButtonComponent,
     ReactiveFormsModule,
@@ -33,7 +35,7 @@ export class ClientDetailComponent implements OnInit {
     this.registerForm = this.fb.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
-      cpf: ['', [Validators.required]],
+      cpf: ['', [Validators.required, this.validateCpf]],
       birthDate: ['', [Validators.required]],
       monthlyIncome: ['', [Validators.required]],
       email: ['', [Validators.required]],
@@ -41,9 +43,52 @@ export class ClientDetailComponent implements OnInit {
     });
   }
 
+  validateCpf(control: any): { [key: string]: any } | null {
+    let cpf = control.value;
+    if (cpf) {
+      let sum = 0;
+      let remainder;
+      cpf = cpf.replace(/[^\d]/g, '');
+      if (cpf === '00000000000') {
+        return { cpfNotValid: true };
+      }
+      for (let i = 1; i <= 9; i++) {
+        sum += parseInt(cpf.substring(i - 1, i)) * (11 - i);
+      }
+      remainder = (sum * 10) % 11;
+      if (remainder === 10 || remainder === 11) {
+        remainder = 0;
+      }
+      if (remainder !== parseInt(cpf.substring(9, 10))) {
+        return { cpfNotValid: true };
+      }
+      sum = 0;
+      for (let i = 1; i <= 10; i++) {
+        sum += parseInt(cpf.substring(i - 1, i)) * (12 - i);
+      }
+      remainder = (sum * 10) % 11;
+      if (remainder === 10 || remainder === 11) {
+        remainder = 0;
+      }
+      if (remainder !== parseInt(cpf.substring(10, 11))) {
+        return { cpfNotValid: true };
+      }
+    }
+    return null;
+  }
+
   onSaveRegister() {
     console.log('save!!!!!');
     console.log(this.registerForm.value);
+    console.log('form is valid?', !this.registerForm.invalid);
+
+    if (this.registerForm.get('cpf')?.invalid) {
+      console.log('O campo CPF é inválido.');
+
+      alert('O CPF é inválido!');
+
+      throw new Error('O CPF é inválido!');
+    }
 
     return new EventEmitter();
   }
